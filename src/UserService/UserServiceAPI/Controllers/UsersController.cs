7392c953099e8de2a167366a7557f18afc5b8826
Michael <bahchevanov.mihail@gmail.com>
2020-10-20 12:55:33 +0200

    :bulb: added refactored users controller (better version)

--- src/UserService/UserServiceAPI/Controllers/UsersController.cs
+++ src/UserService/UserServiceAPI/Controllers/UsersController.cs
@@ -1,17 +1,101 @@
-using Sweepi.UserServiceAPI.Models;
-using Sweepi.UserServiceAPI.Data;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
 using Sweepi.UserServiceAPI.Repository;
+using Sweepi.UserServiceAPI.Mappers;
+using Sweepi.UserServiceAPI.Models;
+using System.Linq;
 
 namespace Sweepi.UserServiceAPI.Contollers
 {
-    [Route("/api/[controller]")]
+    [Route("api/[controller]")]
     [ApiController]
-    public class UsersController : BaseController<User, UserRepository>
+    public class UsersController : ControllerBase
     {
-      public UsersController(UserRepository repository) : base(repository)
+      UserRepository _repository;
+      public UsersController(UserRepository repository)
       {
-          
+          _repository = repository;
+      }
+
+      [HttpGet]
+      public async Task<ActionResult<IEnumerable<User>>> Get()
+      {
+        IEnumerable<User> users = await _repository.GetAll();
+
+        return Ok(users.Select(u => u.MapToDTO()));
+      }
+
+      [HttpGet]
+      [Route("{id}", Name = "GetById")]
+      public async Task<ActionResult<User>> Get(string id)
+      {
+        var user = await _repository.GetById(id);
+
+        if (user == null) return NotFound();
+
+        return Ok(user.MapToDTO());
+      }
+
+      [HttpPut]
+      [Route("{id}")]
+      public async Task<ActionResult<User>> Put(string id, User user)
+      {
+        try
+        {
+            if (user.Id != id) return BadRequest();
+
+            await _repository.Update(user);
+
+            return NoContent();
+        }
+        catch (DbUpdateException)
+        {
+            ModelState.AddModelError("", "Unable to save changes");
+            return StatusCode(StatusCodes.Status500InternalServerError);
+            throw;
+        }
+      }
+
+      [HttpPost]
+      public async Task<ActionResult<User>> Post(User user)
+      {
+        try
+        {
+          if (ModelState.IsValid)
+          {
+            await _repository.Create(user);
+            return CreatedAtRoute("GetById", new { id = user.Id }, user);
+          }
+          return BadRequest();
+        } 
+        catch (DbUpdateException) {
+          ModelState.AddModelError("", "Unable to save changes");
+          return StatusCode(StatusCodes.Status500InternalServerError);
+          throw;
+        }
+      }
+
+      [HttpDelete]
+      [Route("{id}")]
+      public async Task<ActionResult<User>> Delete(string id)
+      {
+        try
+        {
+            var user = await _repository.Delete(id);
+
+            if (user == null) return NotFound();
+
+            return NoContent();
+        }
+        catch (DbUpdateException)
+        {
+            ModelState.AddModelError("", "Unable to save changes");
+            return StatusCode(StatusCodes.Status500InternalServerError);
+            throw;
+        }
       }
     }
 }
