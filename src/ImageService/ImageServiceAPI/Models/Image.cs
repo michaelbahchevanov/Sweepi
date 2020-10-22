68ecbae090c8a746a79bc998576c04e6452d8b97
Michael <bahchevanov.mihail@gmail.com>
2020-10-22 20:33:03 +0200

    :sparkles: added image model

---
+++ src/ImageService/ImageServiceAPI/Models/Image.cs
@@ -0,0 +1,15 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Sweepi.ImageServiceAPI.Models
+{
+    public class Image
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string? UserId { get; set; }
+        public string? ProductId { get; set; }
+        public byte[] ContentImage { get; set; }
+    }
+}
