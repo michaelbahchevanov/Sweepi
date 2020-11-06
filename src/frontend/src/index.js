75609b97516a0df94ad3e74f8babac48b5dbc932
Michael <bahchevanov.mihail@gmail.com>
2020-11-06 12:17:49 +0100

    :sparkles: added initial react app :sparkles:

---
+++ src/frontend/src/index.js
@@ -0,0 +1,12 @@
+import React, { StrictMode } from 'react';
+import ReactDOM from 'react-dom';
+import { App } from './components';
+
+const root = document.getElementById('root');
+
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  root
+);
