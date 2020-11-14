8ab82b083a59e5b57d680edb310f9d0f666d1d08
Michael <bahchevanov.mihail@gmail.com>
2020-11-14 13:08:25 +0100

    :sparkles: added mock login and register views

---
+++ src/frontend/src/components/App/App.js
@@ -0,0 +1,12 @@
+import { RegisterForm, LoginForm } from '../';
+
+function App() {
+  return (
+    <>
+      <LoginForm />
+      <RegisterForm />
+    </>
+  );
+}
+
+export default App;
--- src/frontend/src/components/index.js
+++ src/frontend/src/components/index.js
@@ -1,1 +1,3 @@
-export { default as App } from './App';
+export { default as App } from './App/App';
+export { default as RegisterForm } from './RegisterForm/RegisterForm';
+export { default as LoginForm } from './LoginForm/LoginForm';
